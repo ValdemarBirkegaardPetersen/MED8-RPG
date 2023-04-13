82a45154527ad829575b8e15ab0abf0635980f06
Valdemar B. Petersen <56254921+ValdemarBirkegaardPetersen@users.noreply.github.com>
2023-04-13 11:22:38 +0200

    Added 3 events

---
+++ Assets/Scripts/Events/MakeInvestmentAtBankEvent.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeInvestmentAtBankEvent : MonoBehaviour
+{
+    // To get and set stats
+    EventUtility eg;
+
+    public string Run()
+    {
+        eg = new EventUtility();
+
+        if (eg.getPatrol() > 0.6f)
+        {
+            return "The bank is closed due to recent disturbances in the city. Multiple guards are patrolling inside.";
+        }
+
+        if (eg.getCurrency() < 5)
+        {
+            return "Sir, we would be happy to help you in your investment journey, but the investment cost is 5 coins";
+        }
+
+        var outcome0 = 100 * (eg.getEntropy() + eg.getEntropy());
+        var outcome1 = 10;
+        var outcome2 = 10;
+        var outcome3 = 2 * eg.getIntelligence();
+
+        var finalOutcome = eg.CalculateOutcome(outcome0, outcome1, outcome2, outcome3);
+        // Debug.Log(finalOutcome);
+
+        if (finalOutcome == 0)
+        {
+            eg.setHealth(eg.getHealth() - 25);
+            eg.setCurrency(0);
+            return "Just as you are about to invest your 5 coins, an armed robbery takes place and all the bank employees and visitors are robbed for all the gold they have. The robbers seem to recognize you. You attempt to resist but you suffer a sword wound and are left with no coin";
+        }
+        else if (finalOutcome == 1)
+        {
+            eg.setCurrency(eg.getCurrency() + 2);
+            return "With help from the bankers you happily invest 5 coins in various short term funds. After some time, the bankers pull out your investment for a slight profit";
+        }
+        else if (finalOutcome == 2)
+        {
+            eg.setCurrency(eg.getCurrency() - 2);
+            return "With help from the bankers you happily invest 5 coins in various short term funds. After some time, the bankers pull out your investment for a slight loss";
+        }
+        else if (finalOutcome == 3)
+        {
+            eg.setCurrency(eg.getCurrency() + 10);
+            return "Using your own expertise you take initiative on some wise short term investment opportunities. You make a huge profit over a very short period of time before pulling your funds";
+        }
+
+        Debug.Log("Error in outcome calculcation");
+        return "404";
+    }
+
+    
+}
---
+++ Assets/Scripts/Events/ReadAtLibraryEvent.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadAtLibraryEvent : MonoBehaviour
+{
+    // To get and set stats
+    EventUtility eg;
+
+    public string Run()
+    {
+        eg = new EventUtility();
+
+        if (eg.getIntelligence() < 3)
+        {
+            return "You grab a book and attempt to read it, but you just cannot make sense of any of the letters";
+            // exit event here
+        }
+
+        var outcome0 = 100 * (eg.getEntropy() + eg.getEntropy());
+        var outcome1 = 10;
+        var outcome2 = 4;
+        var outcome3 = 4;
+    
+        var finalOutcome = eg.CalculateOutcome(outcome0, outcome1, outcome2, outcome3);
+        // Debug.Log(finalOutcome);
+
+        if (finalOutcome == 0)
+        {
+            eg.setStrength(eg.getStrength() - 3);
+            eg.setIntelligence(eg.getIntelligence() - 3);
+            eg.setHealth(eg.getHealth() - 5);
+            return "You stumble upon a black humming book, you are blinded by your pursuit of knowledge as you foolishly start reading being absorbed more and more until you pass out. The library helps you recover, but the cursed book has drained you physically and mentally.";
+        }
+        else if (finalOutcome == 1)
+        {
+            eg.setIntelligence(eg.getIntelligence() + 1);
+            return "You grab a book and start reading for hours on end. You end up leaving feeling you learned something new.";
+        }
+        else if (finalOutcome == 2)
+        {
+            eg.setIntelligence(eg.getIntelligence() + 3);
+            eg.setStrength(eg.getStrength() - 1);
+            eg.setHealth(eg.getHealth() - 10);
+            return "You find a huge ancient dusty tome hidden away. Curious you dust it off and carry the heavy book to a nearby table spraining a muscle in the process. But reading through the book you uncover hidden life knowledge.";
+        }
+        else if (finalOutcome == 3)
+        {
+            eg.setCurrency(eg.getCurrency() - 5);
+            eg.setCharisma(eg.getCharisma() - 1);
+            eg.setIntelligence(eg.getIntelligence() + 1);
+            return "You grab a book and start reading for hours on end learning something new. Just as you are about to leave you accidentally spill water on the book. The library workers are upset and you end up paying some coins in restitution.";
+        }
+
+        Debug.Log("Error in outcome calculcation");
+        return "404";
+    }
+
+    
+}
